f748adfa53ac62293dd6ef27eabb0a7084e2ba71
cmtrc <comtruice@gmail.com>
2019-07-02 09:09:48 +0200

    when color is correct, change remaining squares to correct color

--- colorGame.js
+++ colorGame.js
@@ -18,14 +18,24 @@ for (var i = 0; squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
     squares[i].addEventListener("click", function(){
         var clickedColor = this.style.backgroundColor;
-        if(pickedColor === clickedColor) {
-            
+        if(pickedColor === clickedColor) {     
             msg = "Correct!";
             messageDisp.textContent = "Correct!";
+            // change remainding colors to pickedColor
+            changeColors(clickedColor);
+            // change headerColor to pickedColor
+
         }else {
             this.style.backgroundColor = "#232323";
             messageDisp.textContent = "Try Again";
         }
     });
 
+}
+
+
+function changeColors(color) {
+    for(let i = 0; i < colors.length; i++){
+        squares[i].style.backgroundColor = color;
+    }   
 }
